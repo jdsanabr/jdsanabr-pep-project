81db2f1997147ee3bde3a628c7f07b1480ebbfd9
Jonathan Sanabria <jonathandavidsanabria@gmail.com>
2023-06-28 23:06:09 +0000

    Added def & custom constructor to MessageService

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,17 @@
 package Service;
 
+import Model.Message;
+import DAO.MessageDAO;
+
 public class MessageService {
+    MessageDAO messageDAO;
+
+    public MessageService() {
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO m) {
+        messageDAO = m;
+    }
     
 }
