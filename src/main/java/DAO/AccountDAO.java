0db6eb050970236dc4793574cfe1cf87b053518e
jdsanabr <57377290+jdsanabr@users.noreply.github.com>
2023-07-02 16:14:54 -0700

    Update AccountDAO.java
    Added and tested helper methods for AccountService userRegistration(). Not fully working (passing 2/4 tests).

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -8,5 +8,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountDAO {
-    
+	
+	//account login
+//	public Account login(Account account) {
+//		return null;
+//	}
+	//
+	
+	//helper method, used by primary methods to look up an account and see if it exists
+	public Account getAccountByUsername(String username) {
+		Connection connection = ConnectionUtil.getConnection();
+		
+		try {
+			String sql = "SELECT * FROM account WHERE username = ?";
+			
+			//debugging:
+	    	//System.out.println("dao file, try block, username: " + username);
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, username);
+			
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			//debugging:
+	    	//System.out.println("dao file, try block, ResultSet: " + rs);
+			
+			if(!rs.next()) {
+				return null;
+			}
+			
+			while(rs.next()) {
+				Account account = new Account(rs.getInt("account_id"),
+						rs.getString("username"), rs.getString("password"));
+				
+				return account;
+			}
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
+	}
+	//end of helper method
+	
+	//helper method
+	public Account insertAccount(Account account) {
+		Connection connection = ConnectionUtil.getConnection();
+		
+		try {
+			String sql = "INSERT INTO account (username, password) values (?, ?)";
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			
+			preparedStatement.setString(1, account.getUsername());
+			preparedStatement.setString(2, account.getPassword());
+			
+			preparedStatement.executeUpdate();
+			
+			ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+			//debugging:
+			//System.out.println("dao, account obj: " + account);
+			System.out.println("dao, pkResultSet: " + pkeyResultSet);
+			if(!pkeyResultSet.next()) {
+				System.out.println("dao, pkResultSet.next(): false");
+			}
+			
+			if(pkeyResultSet.next()) {
+				int generatedId = (int) pkeyResultSet.getLong(1);
+				//debugging:
+				System.out.println("dao, generatedId: " + generatedId);
+				
+				return new Account(generatedId, account.getUsername(), account.getPassword());
+			}
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
+	}
+	//
+		
 }
