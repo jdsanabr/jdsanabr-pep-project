23a0364ee9b5bfb4eae90e149e5e686fb702b2b4
jdsanabr <57377290+jdsanabr@users.noreply.github.com>
2023-07-03 16:56:22 -0700

    Update AccountService.java

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,84 +1,48 @@
-package DAO;
+package Service;
 
 import Model.Account;
-import Util.ConnectionUtil;
 
-import java.sql.*; //(Connection, PreparedStatement, ResultSet)
+import DAO.AccountDAO;
 
-public class AccountDAO {
+public class AccountService {
+	AccountDAO accountDAO;
 	
-	//helper method, used by primary methods to look up an account and see if it exists
-	public Account getAccountByUsername(String username) {
-		Connection connection = ConnectionUtil.getConnection();
-		
-		try {
-			String sql = "SELECT * FROM account WHERE username = ?";
-			
-			//debugging:
-	    	//System.out.println("dao file, try block, username: " + username);
-			
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			
-			preparedStatement.setString(1, username);
-			
-			ResultSet rs = preparedStatement.executeQuery();
-			
-			//debugging:
-	    	//System.out.println("dao file, try block, ResultSet: " + rs);
-			
-			if(!rs.next()) {
-				return null;
-			}
-			
-			while(rs.next()) {
-				Account account = new Account(rs.getInt("account_id"),
-						rs.getString("username"), rs.getString("password"));
-				
-				return account;
-			}
-		} catch(SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		return null;
+	public AccountService() {
+		accountDAO = new AccountDAO();
 	}
-	//end of helper method
 	
-	//helper method
-	public Account insertAccount(Account account) {
-		Connection connection = ConnectionUtil.getConnection();
-		
-		try {
-			String sql = "INSERT INTO account (username, password) values (?, ?)";
-			
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			
-			preparedStatement.setString(1, account.getUsername());
-			preparedStatement.setString(2, account.getPassword());
-			
-			preparedStatement.executeUpdate();
-			
-			ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
-			//debugging:
-			//System.out.println("dao, account obj: " + account);
-			System.out.println("dao, pkResultSet: " + pkeyResultSet);
-			if(!pkeyResultSet.next()) {
-				System.out.println("dao, pkResultSet.next(): false");
-			}
-			
-			if(pkeyResultSet.next()) {
-				int generatedId = (int) pkeyResultSet.getLong(1);
-				//debugging:
-				System.out.println("dao, generatedId: " + generatedId);
-				
-				return new Account(generatedId, account.getUsername(), account.getPassword());
-			}
-		} catch(SQLException e) {
-			System.out.println(e.getMessage());
+	public AccountService(AccountDAO a) {
+		accountDAO = a;
+	}
+	
+	//account login
+	public Account login(Account account) {
+		//REQ: successful if the username and password provided
+		//in the request body JSON match a real account existing on the database
+		if(accountDAO.getAccountByUsername(account.getUsername()) != null
+				&& accountDAO.getPassword(account) != null) {
+//			System.out.println("account: " + account);
+//			System.out.println("getAccount: " + accountDAO.getAccountByUsername(account.getUsername()));
+			return accountDAO.getAccountByUsername(account.getUsername());
 		}
 		
 		return null;
 	}
 	//
+	
+	//user registration
+	public Account userRegistration(Account account) {
+		//REQ: successful if the username is not blank,
+		//password is at least 4 chars long,
+		//and an account with that username DNE already
 		
+		if(!account.username.isEmpty() && account.password.length() >= 4
+				&& accountDAO.getAccountByUsername(account.getUsername()) == null) {
+			return accountDAO.insertAccount(account);
+		}
+		
+		return null;
+	}
+	//
+    
 }
